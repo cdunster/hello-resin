6f8f9b0a41dba4bb53e7cddb7db8741e74c530ec
cdunster <cdunster@users.noreply.github.com>
2018-10-15 20:24:48 +0200

    Add test for GET on root.

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,29 @@ extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!\nThis page is brought to you from my RPi3."
+    "Hello, World!"
+}
+
+fn create_rocket_with_mounts() -> rocket::Rocket {
+    rocket::ignite().mount("/", routes![index])
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    create_rocket_with_mounts().launch();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rocket::http::ContentType;
+    use rocket::local::Client;
+
+    #[test]
+    fn get_index_returns_content() {
+        let client = Client::new(create_rocket_with_mounts()).unwrap();
+        let mut response = client.get("/").header(ContentType::JSON).dispatch();
+        let body = response.body_string().unwrap();
+
+        assert_eq!("Hello, World!", body);
+    }
 }
