8f69413370a085e2f4df78411a64727d450c3ecb
cdunster <cdunster@users.noreply.github.com>
2018-10-15 21:04:17 +0200

    Add GET zones that returns JSON.

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,25 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
+
+use rocket_contrib::Json;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, World!"
 }
 
+#[get("/zones")]
+fn get_zones() -> Json {
+    Json(json!({
+        "zones": []
+    }))
+}
+
 fn create_rocket_with_mounts() -> rocket::Rocket {
-    rocket::ignite().mount("/", routes![index])
+    rocket::ignite().mount("/", routes![index, get_zones])
 }
 
 fn main() {
@@ -29,5 +40,17 @@ mod tests {
         let body = response.body_string().unwrap();
 
         assert_eq!("Hello, World!", body);
+    }
+
+    #[test]
+    fn given_no_zones_when_get_zones_then_return_object_with_empty_array() {
+        let client = Client::new(create_rocket_with_mounts()).unwrap();
+        let mut response = client.get("/zones").header(ContentType::JSON).dispatch();
+        let body = response.body_string().unwrap();
+
+        let expected = Json(json!({
+            "zones": []
+        })).to_string();
+        assert_eq!(expected, body);
     }
 }
