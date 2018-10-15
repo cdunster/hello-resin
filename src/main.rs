d9374f7bf467fbf83847d8ba26d1061a2b766b48
cdunster <cdunster@users.noreply.github.com>
2018-10-15 21:11:36 +0200

    Update test name.

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,7 @@ mod tests {
     }
 
     #[test]
-    fn given_no_zones_when_get_zones_then_return_object_with_empty_array() {
+    fn given_no_zones_when_get_zones_then_return_json_object_with_empty_array() {
         let client = Client::new(create_rocket_with_mounts()).unwrap();
         let mut response = client.get("/zones").header(ContentType::JSON).dispatch();
         let body = response.body_string().unwrap();
